5aa1866badb7dbb1309fd3076aae0e9a6debe245
Hendy Irawan <ceefour666@gmail.com>
2015-04-23 16:09:57 +0700

    bisa nongolin tulisan untuk speech

---
+++ Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceCamera : MonoBehaviour {
+
+	public GameObject theCamera;
+	public GameObject avatar;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = avatar.transform.position + new Vector3 (0.5f, 1f, -1f);
+		transform.LookAt(transform.localPosition + theCamera.transform.rotation * -Vector3.back,
+		                 theCamera.transform.rotation * Vector3.up);
+	}
+}
